12817108cb4fccd2370b6b3dc46469399c1379ec
Ignat Remizov <ignat980@gmail.com>
2015-11-07 17:08:25 -0800

    Initial Commit

---
+++ DynamicLibraryLoadTracker/DynamicLibraryLoadTracker.h
@@ -0,0 +1,19 @@
+//
+//  DynamicLibraryLoadTracker.h
+//  DynamicLibraryLoadTracker
+//
+//  Created by Ignat Remizov on 11/7/15.
+//  Copyright © 2015 Ignat Remizov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicLibraryLoadTracker.
+FOUNDATION_EXPORT double DynamicLibraryLoadTrackerVersionNumber;
+
+//! Project version string for DynamicLibraryLoadTracker.
+FOUNDATION_EXPORT const unsigned char DynamicLibraryLoadTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicLibraryLoadTracker/PublicHeader.h>
+
+
